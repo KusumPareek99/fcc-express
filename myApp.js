0a093256b88ea07f707d2956a01188ee607fda32
KusumPareek99 <kusumpareek7620@gmail.com>
2023-12-26 10:03:51 +0530

    added static middleware for public assets

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require("express");
 let app = express();
 console.log("Hello World");
+let path = __dirname + "/public";
+const middlewareFunction = express.static(path);
+app.use(middlewareFunction);
+app.use("/public", middlewareFunction);
 
 app.get("/", (req, res) => {
   //   res.send("Hello Express");
