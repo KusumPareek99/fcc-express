88d932e76d353c1de5a22a1c66b6b04bfe54f5d2
KusumPareek99 <kusumpareek7620@gmail.com>
2023-12-26 10:46:05 +0530

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -29,4 +29,15 @@ app.get("/json", (req, res) => {
   //  res.json({ message: "Hello json" });
 });
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({ time: req.time });
+  }
+);
+
 module.exports = app;
